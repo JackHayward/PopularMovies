f8fe02266e575b2abdc9ff23d740eade95bcb57a
Jack Hayward <38814148+JackHayward@users.noreply.github.com>
2020-08-12 16:05:13 +0100

    Commit before trying new approach

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.popularmovies;
 
+import android.icu.text.RelativeDateTimeFormatter;
+import android.os.AsyncTask;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.squareup.picasso.Picasso;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    new RetrieveMovies().execute("sdsdsd");
+  }
+
+  public static class RetrieveMovies extends AsyncTask<String, Void, String> {
+
+    @Override protected String doInBackground(String... strings) {
+      String string = null;
+
+      URL url = NetworkUtils.buildUrl("popularity");
+      try {
+        string = NetworkUtils.getResponseFromHttpUrl(url);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      return string;
+    }
   }
 }
