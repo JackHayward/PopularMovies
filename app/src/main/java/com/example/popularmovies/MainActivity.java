510d22413da96965a5ec461f117a1038ed189126
Jack Hayward <38814148+JackHayward@users.noreply.github.com>
2020-08-19 20:36:05 +0100

    Application finished. Sorting now works

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.popularmovies;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -18,11 +22,12 @@ public class MainActivity extends AppCompatActivity {
   private RecyclerView recyclerView;
   private RecyclerView.Adapter adapter;
   private RecyclerView.LayoutManager layoutManager;
+  private static int currentSortMode = 1;
   private static String baseUrl = "http://api.themoviedb.org";
   private static final String language = "en-US";
   private static final int page = 1;
   private static final String apiKey = "1c0e9c5248204984a257ab3dd013c1a1";
-  private static final String category = "popular";
+  private static String category = "popular";
 
   public MainActivity() {
   }
@@ -32,9 +37,42 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    loadPage(1);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.sort_menu, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.sort_by_popularity:
+        currentSortMode = 1;
+        break;
+      case R.id.sort_by_top:
+        currentSortMode = 2;
+        break;
+    }
+
+    loadPage(1);
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void loadPage(final int page) {
     recyclerView = findViewById(R.id.recyclerView);
     recyclerView.setHasFixedSize(true);
     layoutManager = new GridLayoutManager(this, 3);
+
+    switch(currentSortMode) {
+      case 1:
+        category = "popular";
+        break;
+      case 2:
+        category = "top_rated";
+        break;
+    }
 
     Retrofit retrofit = new Retrofit.Builder()
         .baseUrl(baseUrl)
