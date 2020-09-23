bf413c10b18182846675060792065767008f9540
Jack Hayward <38814148+JackHayward@users.noreply.github.com>
2020-09-23 19:21:42 +0100

    Favourite POJO with entity made

--- app/src/main/java/com/example/popularmovies/models/Favourite.java
+++ app/src/main/java/com/example/popularmovies/models/Favourite.java
@@ -1,16 +1,76 @@
 package com.example.popularmovies.models;
 
 import android.provider.BaseColumns;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity
 public class Favourite {
 
-  public static final class FavouriteEntry implements BaseColumns {
+  @PrimaryKey(autoGenerate = true)
+  private int id;
 
-    public static final String TABLE_NAME = "favourite";
-    public static final String COLUMN_MOVIEID = "movieid";
-    public static final String COLUMN_TITLE = "title";
-    public static final String COLUMN_USERRATING = "userrating";
-    public static final String COLUMN_POSTER_PATH = "posterpath";
-    public static final String COLUMN_PLOT_SYNOPSIS = "overview";
+  @ColumnInfo(name = "movie_id")
+  private String movieId;
+
+  @ColumnInfo(name = "movie_title")
+  private String movieTitle;
+
+  @ColumnInfo(name = "user_rating")
+  private String userRating;
+
+  @ColumnInfo(name = "poster_path")
+  private String posterPath;
+
+  @ColumnInfo(name = "synopsis")
+  private String synopsis;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getMovieId() {
+    return movieId;
+  }
+
+  public String getMovieTitle() {
+    return movieTitle;
+  }
+
+  public String getUserRating() {
+    return userRating;
+  }
+
+  public String getPosterPath() {
+    return posterPath;
+  }
+
+  public String getSynopsis() {
+    return synopsis;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setMovieId(String movieId) {
+    this.movieId = movieId;
+  }
+
+  public void setMovieTitle(String movieTitle) {
+    this.movieTitle = movieTitle;
+  }
+
+  public void setUserRating(String userRating) {
+    this.userRating = userRating;
+  }
+
+  public void setPosterPath(String posterPath) {
+    this.posterPath = posterPath;
+  }
+
+  public void setSynopsis(String synopsis) {
+    this.synopsis = synopsis;
   }
 }
